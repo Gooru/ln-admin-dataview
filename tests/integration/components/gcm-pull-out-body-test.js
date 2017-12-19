4df06a44ed2cff8a968be6174c6c5980e0ee7aa9
Gugan <gugankumar.p@bambeeq.com>
2017-12-19 16:58:56 +0530

    Renamed reference text

--- tests/integration/components/gcm-pull-out-body-test.js
+++ tests/integration/components/gcm-pull-out-body-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('app-pull-out-body', 'Integration | Component | app pull out body', {
+moduleForComponent('gcm-pull-out-body', 'Integration | Component | gcm pull out body', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{app-pull-out-body}}`);
+  this.render(hbs`{{gcm-pull-out-body}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#app-pull-out-body}}
+    {{#gcm-pull-out-body}}
       template block text
-    {{/app-pull-out-body}}
+    {{/gcm-pull-out-body}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
