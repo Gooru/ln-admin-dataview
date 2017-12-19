4f285f3ecd37bc6286bf5df300365be7dc0c5fd9
ashokkumar <ashok.s@bambeeq.com>
2017-12-19 11:23:53 +0530

    review changes added

--- tests/integration/components/content-display-test.js
+++ tests/integration/components/content-display-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('gcm-content-count', 'Integration | Component | gcm content count', {
+moduleForComponent('content-display', 'Integration | Component | gcm content count', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{gcm-content-count}}`);
+  this.render(hbs`{{content-display}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#gcm-content-count}}
+    {{#content-display}}
       template block text
-    {{/gcm-content-count}}
+    {{/content-display}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
