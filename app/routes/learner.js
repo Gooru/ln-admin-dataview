7cb2f53258b1d2987ea16412955121e466d5a5b5
sheeban <sheeban@gooru.org>
2018-01-16 15:44:08 +0530

    added support for profile bar

--- app/routes/learner.js
+++ app/routes/learner.js
@@ -1,7 +1,35 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'admin-dataview/mixins/authenticated-route-mixin';
+import {LEARNER_CHILD_ROUTES} from 'admin-dataview/config/config';
+import Utils from 'admin-dataview/utils/utils';
+
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+
+  //------------------------------------------------------------------------
+  //Properties
+
+  //------------------------------------------------------------------------
+  // Events
+
+  actions: {
+
+
+    onClickBackButton: function() {
+      let pathname = Utils.getRoutePathLastOccurrence(window.location.pathname);
+      if (LEARNER_CHILD_ROUTES.indexOf(pathname) > -1) {
+        let controller = this.get('controller');
+        this.transitionTo('learner', controller.get('userId'));
+      } else {
+        this.transitionTo('learners');
+      }
+
+    }
+  },
+
+  //------------------------------------------------------------------------
+  //Methods
 
   model: function(params) {
     return params;
--- app/routes/learner/index.js
+++ app/routes/learner/index.js
@@ -15,27 +15,39 @@ export default Ember.Route.extend({
   //------------------------------------------------------------------------
   //Properties
 
+  /**
+   * User id of the learner
+   * @type {String}
+   */
+  userId: null,
 
   // -------------------------------------------------------------------------
   // Methods
 
-  model: function(params) {
+  beforeModel: function(transition) {
+    this.set('userId', transition.params.learner.userId);
+  },
+
+
+  model: function() {
+    let userId = this.get('userId');
     return Ember.RSVP.hash({
       learnersProfileDistribution: this.get('learnersService').getLearnerProfileDistribution(),
-      userStatsContent: this.get('learnersService').getUserStatsContent(params.userId),
-      userStatsByCourse: this.get('learnersService').getUserStatsByCourse(params.userId),
-      userJourneyStats: this.get('learnersService').getUserJourneyStats(params.userId),
-      userCompetencyStats: this.get('learnersService').getUserCompetencyStats(params.userId)
+      userStatsContent: this.get('learnersService').getUserStatsContent(userId),
+      userStatsByCourse: this.get('learnersService').getUserStatsByCourse(userId),
+      userJourneyStats: this.get('learnersService').getUserJourneyStats(userId),
+      userCompetencyStats: this.get('learnersService').getUserCompetencyStats(userId)
     });
   },
 
 
   setupController: function(controller, model) {
+    let userId = this.get('userId');
     controller.set('userJourneyStats', model.userJourneyStats);
     controller.set('competenciesData', model.userCompetencyStats);
     controller.set('userStatsContent', model.userStatsContent);
     controller.set('userStatsByCourse', model.userStatsByCourse);
-    controller.set('userId', model.userId);
+    controller.set('userId', userId);
   }
 
 });
