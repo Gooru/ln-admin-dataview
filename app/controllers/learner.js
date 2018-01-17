3b01004167b84423815630e22b9462fb684db2e1
sheeban <sheeban@gooru.org>
2018-01-17 12:29:50 +0530

    added fix redirect

--- app/controllers/learner.js
+++ app/controllers/learner.js
@@ -1,20 +1,32 @@
 import Ember from 'ember';
 import ModalMixin from 'admin-dataview/mixins/modal';
+import {LEARNER_CHILD_ROUTES} from 'admin-dataview/config/config';
+import Utils from 'admin-dataview/utils/utils';
+
 
 export default Ember.Controller.extend(ModalMixin, {
 
   // --------------------------------------------------------------------------
   // Query params
 
-  // --------------------------------------------------------------------------
-  // Actions
+  //------------------------------------------------------------------------
+  // Events
 
-  actions:  {
+  actions: {
 
+    onClickBackButton: function() {
+      let pathname = Utils.getRoutePathLastOccurrence(window.location.pathname);
+      if (LEARNER_CHILD_ROUTES.indexOf(pathname) > -1) {
+        this.transitionToRoute('learner', this.get('userId'));
+      } else {
+        this.transitionToRoute('learners');
+      }
+    }
   }
 
   // --------------------------------------------------------------------------
   // Properties
+
 
 
 });
--- app/routes/learner.js
+++ app/routes/learner.js
@@ -1,7 +1,5 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'admin-dataview/mixins/authenticated-route-mixin';
-import {LEARNER_CHILD_ROUTES} from 'admin-dataview/config/config';
-import Utils from 'admin-dataview/utils/utils';
 
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
@@ -20,22 +18,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   //------------------------------------------------------------------------
   //Properties
 
-  //------------------------------------------------------------------------
-  // Events
 
-  actions: {
-
-    onClickBackButton: function() {
-      let pathname = Utils.getRoutePathLastOccurrence(window.location.pathname);
-      if (LEARNER_CHILD_ROUTES.indexOf(pathname) > -1) {
-        let controller = this.get('controller');
-        this.transitionTo('learner', controller.get('userId'));
-      } else {
-        this.transitionTo('learners');
-      }
-
-    }
-  },
 
   //------------------------------------------------------------------------
   //Methods
@@ -51,7 +34,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
 
   setupController: function(controller, model) {
-    controller.set('userId', model.userProfile.id);
+    controller.set('userId', model.userProfile.userId);
     controller.set('user', model.userProfile);
   }
 
