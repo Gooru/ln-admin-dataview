10ac42e9f2a2fc9cbd7ba6a3dea22dc52d11424c
ashokkumar <ashok.s@bambeeq.com>
2018-01-17 09:46:37 +0530

    NILE-2372 accordion menu

---
+++ app/components/learners/accordion-summary.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['learners', 'accordion-summary'],
+
+
+  classNameBindings: ['isExpanded:expanded'],
+
+
+  // -------------------------------------------------------------------------
+  // Actions
+  actions: {
+    /**
+     *
+     * Triggered when an menu item is selected
+     * @param item
+     */
+    selectComptency: function(item) {
+      //here write the selected competency details
+    }
+  },
+
+  // -------------------------------------------------------------------------
+  // Events
+
+  setupComponent: Ember.on('didInsertElement', function() {
+    const component = this;
+
+    this.$().on('hide.bs.collapse', function(e) {
+      e.stopPropagation();
+      component.set('isExpanded', false);
+    });
+
+    this.$().on('show.bs.collapse', function(e) {
+      e.stopPropagation();
+      component.set('isExpanded', true);
+    });
+
+    Ember.run.scheduleOnce('afterRender', this, this.parsedLocationChanged);
+  }),
+
+  removeSubscriptions: Ember.on('willDestroyElement', function() {
+    this.$().off('hide.bs.collapse');
+    this.$().off('show.bs.collapse');
+  })
+
+});
