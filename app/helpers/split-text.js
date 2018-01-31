53e6170b760244742aefdc38bba5a0033693a47b
sheeban <sheeban@gooru.org>
2018-01-31 13:25:44 +0530

    fixed eslint error

--- app/helpers/split-text.js
+++ app/helpers/split-text.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
-
+/**
+ * Purpose of  this helper is to split the text based on given delimiter
+ * and return first occurrence of splitted text
+ * @param  {Array} params
+ * @return {String}
+ */
 export function splitText(params) {
   let text = params[0];
   let delimiter = params[1];
