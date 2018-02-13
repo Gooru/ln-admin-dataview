0456b2db6ed77bafa52374ffdc707ff6dd7da9c0
sheeban <sheeban@gooru.org>
2018-02-13 19:31:58 +0530

    updated cards for activies search cards

--- app/components/activity/course-card.js
+++ app/components/activity/course-card.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // -------------------------------------------------------------------------
+  // Attributes
+
+  classNames: ['activity-course-card']
+
+  // -------------------------------------------------------------------------
+  // Events
+
 });
