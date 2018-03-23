d6ec0569768352732e71cabdb6a5c218f2a6bc9f
sheeban <sheeban@gooru.org>
2018-03-23 18:39:09 +0530

    changed default landing to tree view

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,12 +6,10 @@ import ConfigurationMixin from 'admin-dataview/mixins/configuration';
  * @typedef {object} ApplicationRoute
  */
 export default Ember.Route.extend(ConfigurationMixin, {
-
   // -------------------------------------------------------------------------
   // Dependencies
 
   i18n: Ember.inject.service(),
-
 
   /**
    * Authentication (api-sdk/session) service.
@@ -61,27 +59,31 @@ export default Ember.Route.extend(ConfigurationMixin, {
     const params = transition.queryParams;
     const route = this;
     let details = null;
-    details = route.get('configurationService').loadConfiguration().then(function() {
-      let accessToken = params.access_token;
-      if (route.get('session.isAuthenticated') && params.access_token) {
-        route.get('session').invalidate();
-      }
-      if (accessToken) {
-        route.get('authService').authenticateWithToken(accessToken).then(function() {
-          route.transitionTo('competency.matrix', {
-            queryParams: {
-              access_token: undefined
-            }
-          });
-        });
-      }
-    });
+    details = route
+      .get('configurationService')
+      .loadConfiguration()
+      .then(function() {
+        let accessToken = params.access_token;
+        if (route.get('session.isAuthenticated') && params.access_token) {
+          route.get('session').invalidate();
+        }
+        if (accessToken) {
+          route
+            .get('authService')
+            .authenticateWithToken(accessToken)
+            .then(function() {
+              route.transitionTo('competency.tree', {
+                queryParams: {
+                  access_token: undefined
+                }
+              });
+            });
+        }
+      });
     return details;
   },
 
   // -------------------------------------------------------------------------
   // Actions - only transition actions should be placed at the route
-  actions: {
-
-  }
+  actions: {}
 });
