03402e2220ec7a374a2ce0d7e4e6f9c8bf074605
Gugan <gugankumar.p@bambeeq.com>
2018-04-27 15:22:15 +0530

    Learning Map: select default item in the domain browser

--- app/components/cards/app-domain-browser-card.js
+++ app/components/cards/app-domain-browser-card.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { LEARNING_MAP_DEFAULT_LEVELS } from 'admin-dataview/config/config';
 
 export default Ember.Component.extend({
   // -------------------------------------------------------------------------
@@ -10,6 +11,44 @@ export default Ember.Component.extend({
   didInsertElement() {
     let component = this;
     component.$('.category .k_12').addClass('active');
+    // this.checkDefaultItem();
+  },
+
+  dataItemObserver: Ember.observer('dataContent', function() {
+    this.checkDefaultItem();
+  }),
+
+  /**
+   * @function checkDefaultItem
+   * Method to set first item in each component selected by default
+   */
+  checkDefaultItem() {
+    let defaultLevels = LEARNING_MAP_DEFAULT_LEVELS;
+    const $categoryComponent = Ember.$('.category .item');
+    const $subjectComponent = Ember.$('.subject .item');
+    const $courseComponent = Ember.$('.course .item');
+    if (!$categoryComponent.hasClass('active')) {
+      Ember.$(`.category .item.${defaultLevels.categoryCode}`).addClass(
+        'active'
+      );
+    }
+    if (!$subjectComponent.hasClass('active')) {
+      Ember.$(
+        `.subject .item.${defaultLevels.subjectCode.replace(/\./, '-')}`
+      ).addClass('active');
+    }
+    if (!$courseComponent.hasClass('active')) {
+      Ember.$(
+        `.course .item.${defaultLevels.courseCode.replace(/\./, '-')}`
+      ).addClass('active');
+    }
+    let domainCodes = defaultLevels.domainCode || null;
+    if (domainCodes) {
+      domainCodes = domainCodes.split(',');
+      domainCodes.map(domainCode => {
+        Ember.$(`.domain .item.${domainCode.replace(/\./, '-')} input`).prop('checked', true);
+      });
+    }
   },
 
   // -------------------------------------------------------------------------
--- app/components/learning-map/app-domain-browser.js
+++ app/components/learning-map/app-domain-browser.js
@@ -30,44 +30,6 @@ export default Ember.Component.extend({
     });
   },
 
-  itemObserver: Ember.observer('subjects', 'courses', 'domains', function() {
-    let component = this;
-    component.checkDefaultItem();
-  }),
-
-  /**
-   * @function checkDefaultItem
-   * Method to set first item in each component selected by default
-   */
-  checkDefaultItem() {
-    let defaultLevels = LEARNING_MAP_DEFAULT_LEVELS;
-    const $categoryComponent = Ember.$('.category .item');
-    const $subjectComponent = Ember.$('.subject .item');
-    const $courseComponent = Ember.$('.course .item');
-    if (!$categoryComponent.hasClass('active')) {
-      Ember.$(`.category .item.${defaultLevels.categoryCode}`).addClass(
-        'active'
-      );
-    }
-    if (!$subjectComponent.hasClass('active')) {
-      Ember.$(
-        `.subject .item.${defaultLevels.subjectCode.replace(/\./, '-')}`
-      ).addClass('active');
-    }
-    if (!$courseComponent.hasClass('active')) {
-      Ember.$(
-        `.course .item.${defaultLevels.courseCode.replace(/\./, '-')}`
-      ).addClass('active');
-    }
-    let domainCodes = defaultLevels.domainCode || null;
-    if (domainCodes) {
-      domainCodes = domainCodes.split(',');
-      domainCodes.map(domainCode => {
-        Ember.$(`.domain .item.${domainCode.replace(/\./, '-')} input`).prop('checked', true);
-      });
-    }
-  },
-
   // -------------------------------------------------------------------------
   // Actions
   actions: {
