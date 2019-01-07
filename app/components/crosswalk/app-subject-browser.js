13e7a7cb9e47f618f952c3dde8bd70dffbc02df9
sheeban <sheeban@gooru.org>
2019-01-07 17:50:03 +0530

    Integrated taxonomy classification API for fw browser and update crosswalk json stubs

--- app/components/crosswalk/app-subject-browser.js
+++ app/components/crosswalk/app-subject-browser.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
-import {TAXONOMY_CATEGORIES} from 'admin-dataview/config/config';
 
 export default Ember.Component.extend({
-
   // -------------------------------------------------------------------------
   // Attributes
 
@@ -11,52 +9,56 @@ export default Ember.Component.extend({
   // -------------------------------------------------------------------------
   // Dependencies
   /**
-  * Search service to fetch content details
-  */
+   * Search service to fetch content details
+   */
   taxonomyService: Ember.inject.service('taxonomy'),
 
   // -------------------------------------------------------------------------
   // Properties
 
   /**
-  * List of categories
-  */
-  categories: TAXONOMY_CATEGORIES,
-
-  /**
-  * List of subjects
-  */
+   * List of subjects
+   */
   subjects: [],
 
   /**
-  * List of frameworks
-  */
+   * List of frameworks
+   */
   frameworks: [],
 
   /**
-  * Selected category id
-  */
+   * Selected category id
+   */
   currentCategoryId: 'k_12',
 
   /**
-  * Selected subject id
-  */
+   * Selected subject id
+   */
   currentSubjectId: null,
 
   /**
-  * Is subject level visible
-  */
+   * Is subject level visible
+   */
   isShowSubjectLevel: false,
 
   /**
-  * Is framework level visible
-  */
+   * Is framework level visible
+   */
   isShowFrameworkLevel: false,
 
   /**
-  * Default category
-  */
-  defaultCategory: 'k_12',
+   * default categories
+   * @type {Array}
+   */
+  categories: Ember.A([]),
+
+  /**
+   * Default category
+   */
+  defaultCategory: Ember.computed('categories', function() {
+    let category = this.get('categories').objectAt(0);
+    return category.get('id');
+  }),
 
   // -------------------------------------------------------------------------
   // Events
@@ -64,20 +66,23 @@ export default Ember.Component.extend({
   init: function() {
     let component = this;
     component._super(...arguments);
-    let currentSubject = component.fetchTaxonomySubjects(component.get('defaultCategory'));
-    currentSubject.then(function(subject) {
-      component.set('currentSubjectId', subject.id);
-      component.fetchTaxonomyFrameworks(subject);
+    component.fetchTaxonomyClassifications().then(() => {
+      let currentSubject = component.fetchTaxonomySubjects(
+        component.get('defaultCategory')
+      );
+      currentSubject.then(function(subject) {
+        component.set('currentSubjectId', subject.id);
+        component.fetchTaxonomyFrameworks(subject);
+      });
     });
   },
 
   // -------------------------------------------------------------------------
   // Actions
   actions: {
-
     /**
-    * Action triggered when user click category to pull subjects
-    */
+     * Action triggered when user click category to pull subjects
+     */
     getSubjects: function(category) {
       let component = this;
       let currentCategoryId = component.get('currentCategoryId');
@@ -90,8 +95,8 @@ export default Ember.Component.extend({
     },
 
     /**
-    * Action triggered when user click subject to pull frameworks
-    */
+     * Action triggered when user click subject to pull frameworks
+     */
     getFrameworks: function(subject) {
       let component = this;
       let category = component.get('currentCategoryId');
@@ -101,8 +106,8 @@ export default Ember.Component.extend({
     },
 
     /**
-    * Action triggered when user select a framework
-    */
+     * Action triggered when user select a framework
+     */
     frameworkStack: function(frameworkId) {
       let component = this;
       let subjectId = component.get('currentSubjectId');
@@ -114,31 +119,49 @@ export default Ember.Component.extend({
   // Methods
 
   /**
-  * @param category
-  * Method to fetchTaxonomySubjects
-  */
+   * @param category
+   * Method to fetchTaxonomySubjects
+   */
   fetchTaxonomySubjects(category) {
     let component = this;
-    const subjectsPromise = Ember.RSVP.resolve(component.get('taxonomyService').getSubjects(category));
+    const subjectsPromise = Ember.RSVP.resolve(
+      component.get('taxonomyService').getSubjects(category)
+    );
     return Ember.RSVP.hash({
       subjectsList: subjectsPromise
-    })
-      .then(function(hash) {
-        component.set('subjects', hash.subjectsList);
-        component.set('isShowFrameworkLevel', false);
-        component.set('isShowSubjectLevel', true);
-        //Use Math as default subject
-        return hash.subjectsList[1];
-      });
+    }).then(function(hash) {
+      component.set('subjects', hash.subjectsList);
+      component.set('isShowFrameworkLevel', false);
+      component.set('isShowSubjectLevel', true);
+      //Use Math as default subject
+      return hash.subjectsList[1];
+    });
   },
 
   /**
-  * @param subject
-  * Method to fetchTaxonomyFrameworks
-  */
+   * @param subject
+   * Method to fetchTaxonomyFrameworks
+   */
   fetchTaxonomyFrameworks(subject) {
     let component = this;
     component.set('frameworks', subject.frameworks);
     component.set('isShowFrameworkLevel', true);
+  },
+
+  /**
+   * Methods used to fetch the taxonomy classifications
+   * @return {Promise}
+   */
+  fetchTaxonomyClassifications() {
+    let component = this;
+    return new Ember.RSVP.Promise(function(resolve) {
+      return component
+        .get('taxonomyService')
+        .getTaxonomyClassifications()
+        .then(classifications => {
+          component.set('categories', classifications);
+          resolve();
+        });
+    });
   }
 });
