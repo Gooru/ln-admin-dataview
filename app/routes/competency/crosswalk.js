d48d3c105098aee928ddcccc03da3a4373f0f807
sheeban <sheeban@gooru.org>
2019-01-07 19:02:22 +0530

    Render category value from API instead of from translation

--- app/routes/competency/crosswalk.js
+++ app/routes/competency/crosswalk.js
@@ -1,17 +1,29 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'admin-dataview/mixins/authenticated-route-mixin';
 
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  // -------------------------------------------------------------------------
+  // Dependencies
+
+  /**
+   * Search service to fetch content details
+   */
+  taxonomyService: Ember.inject.service('taxonomy'),
+
+  model() {
+    return Ember.RSVP.hash({
+      categories: this.get('taxonomyService').getTaxonomyClassifications()
+    });
+  },
 
   //Methods
   //-------------------------------------------------------------------------
 
-  setupController: function(controller) {
+  setupController: function(controller, model) {
     controller.set('enableGenerateTableBtn', false);
     controller.set('showCrosswalkTable', false);
     controller.set('showSubjectBrowser', true);
     controller.set('selectedFrameworks', []);
+    controller.set('categories', model.categories);
   }
-
 });
