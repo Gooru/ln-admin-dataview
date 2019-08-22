806ebacca2c19c56efada06ae275db1a31830585
sheeban <sheeban@gooru.org>
2019-08-22 22:30:01 +0530

    Ui improvements

--- app/components/charts/world-map.js
+++ app/components/charts/world-map.js
@@ -8,7 +8,6 @@ import Ember from 'ember';
 import d3 from 'd3';
 
 export default Ember.Component.extend({
-
   //------------------------------------------------------------------------
   //Dependencies
 
@@ -84,104 +83,140 @@ export default Ember.Component.extend({
     let width = component.get('width');
     let height = component.get('height');
     let keyName = component.get('keyName');
-    let projection = d3.geoEquirectangular()
+    let projection = d3
+      .geoEquirectangular()
       .scale(height / Math.PI)
       .translate([width / 2, height / 2]);
-    let path = d3.geoPath()
-      .projection(projection);
+    let path = d3.geoPath().projection(projection);
 
-    let svg = d3.select(component.element).insert('svg')
+    let svg = d3
+      .select(component.element)
+      .insert('svg')
       .attr('width', width)
       .attr('height', height);
 
-    let states = svg.append('svg:g')
-      .attr('id', 'states');
+    let states = svg.append('svg:g').attr('id', 'states');
 
-    let circles = svg.append('svg:g')
-      .attr('id', 'circles');
+    let circles = svg.append('svg:g').attr('id', 'circles');
 
+    let labels = svg.append('svg:g').attr('id', 'labels');
 
-    let labels = svg.append('svg:g')
-      .attr('id', 'labels');
-
-    component.get('countriesService').getCountries().then((collection) => {
-      states.selectAll('path')
-        .data(collection.features)
-        .enter().append('svg:path')
-        .attr('d', path);
-    });
-
-    component.get('countriesService').getCountriesRegion().then((region) => {
-      let locationDataCount = component.get('locationDataCount');
-      locationDataCount.forEach(data => {
-        let countryRegion = region.findBy('code', data.get('code'));
-        data.set('longitude', countryRegion.get('longitude'));
-        data.set('latitude', countryRegion.get('latitude'));
-        data.set('percentage', countryRegion.get('totalPercentage'));
+    component
+      .get('countriesService')
+      .getCountries()
+      .then(collection => {
+        states
+          .selectAll('path')
+          .data(collection.features)
+          .enter()
+          .append('svg:path')
+          .attr('d', path);
       });
-      circles.selectAll('circle')
-        .data(locationDataCount)
-        .enter()
-        .append('svg:circle')
-        .attr('cx', function(d) {
-          let longitude = d.get('longitude');
-          let latitude = d.get('latitude');
-          return projection([longitude, latitude])[0];
-        })
-        .attr('cy', function(d) {
-          let longitude = d.get('longitude');
-          let latitude = d.get('latitude');
-          return projection([longitude, latitude])[1];
-        })
-        .attr('r', function(d) {
-          let count = d.get(keyName);
-          return keyName === 'active' ? (count) * component.get('bubbleScalefactor') : count / 100000 * d.get('percentage');
-        });
 
-      labels.selectAll('labels')
-        .data(locationDataCount)
-        .enter()
-        .append('svg:text')
-        .attr('x', function(d) {
-          let longitude = d.get('longitude');
-          let latitude = d.get('latitude');
-          return projection([longitude, latitude])[0];
-        })
-        .attr('y', function(d) {
-          let longitude = d.get('longitude');
-          let latitude = d.get('latitude');
-
-          return projection([longitude, latitude])[1];
-        })
-        .attr('dy', function(d) {
-          let count = d.get(keyName);
-          let size = Math.round((count) * component.get('bubbleTextScalefactor'));
-          //TODO should update the logic of calculatiing dy position
-          return keyName === 'active' ? (size / 2) : 0;
-        })
-        .attr('text-anchor', 'middle')
-        .attr('style', function(d) {
-          let count = d.get(keyName);
-          //TODO should update the logic of calculatiing font size
-          let size = keyName === 'active' ? Math.round((count) * component.get('bubbleTextScalefactor')) : 16;
-          return `font-size:${ size   }px`;
-        }).text(function(d) {
-          let count = d.get(keyName);
-          //TODO change text calculation logic
-          return keyName === 'active' ? component.dataCountFormat(count) : component.dataCountFormat(count / 100 * d.get('percentage'));
+    component
+      .get('countriesService')
+      .getCountriesRegion()
+      .then(region => {
+        let locationDataCount = component.get('locationDataCount');
+        locationDataCount.forEach(data => {
+          let countryRegion = region.findBy('code', data.get('code'));
+          data.set('longitude', countryRegion.get('longitude'));
+          data.set('latitude', countryRegion.get('latitude'));
+          data.set('percentage', countryRegion.get('totalPercentage'));
         });
-    });
+        circles
+          .selectAll('circle')
+          .data(locationDataCount)
+          .enter()
+          .append('svg:circle')
+          .attr('cx', function(d) {
+            let longitude = d.get('longitude');
+            let latitude = d.get('latitude');
+            return projection([longitude, latitude])[0];
+          })
+          .attr('cy', function(d) {
+            let longitude = d.get('longitude');
+            let latitude = d.get('latitude');
+            return projection([longitude, latitude])[1];
+          })
+          .attr('r', function(d) {
+            const count = d.get(keyName);
+            const numberOfDigits = count.toString().length;
+            return component.circleRadius(numberOfDigits);
+          });
+
+        labels
+          .selectAll('labels')
+          .data(locationDataCount)
+          .enter()
+          .append('svg:text')
+          .attr('x', function(d) {
+            let longitude = d.get('longitude');
+            let latitude = d.get('latitude');
+            return projection([longitude, latitude])[0];
+          })
+          .attr('y', function(d) {
+            let longitude = d.get('longitude');
+            let latitude = d.get('latitude');
+
+            return projection([longitude, latitude])[1] + 5;
+          })
+          .attr('dy', function(d) {
+            let count = d.get(keyName);
+            let size = Math.round(
+              count * component.get('bubbleTextScalefactor')
+            );
+            //TODO should update the logic of calculatiing dy position
+            return keyName === 'active' ? size / 2 : 0;
+          })
+          .attr('text-anchor', 'middle')
+          .attr('style', function(d) {
+            let count = d.get(keyName);
+            //TODO should update the logic of calculatiing font size
+            let size =
+              keyName === 'active'
+                ? Math.round(count * component.get('bubbleTextScalefactor'))
+                : 16;
+            return `font-size:${size}px`;
+          })
+          .text(function(d) {
+            let count = d.get(keyName);
+            //TODO change text calculation logic
+            const numberOfDigits = count.toString().length;
+            return numberOfDigits > 4 ? component.dataCountFormat(count) : '';
+          });
+      });
   },
 
-  dataCountFormat: function(count) {
-    return Math.abs(Number(count)) >= 1.0e+9 ?
-      `${Math.round(Math.abs(Number(count)) / 1.0e+9)  }B` :
-      Math.round(Math.abs(Number(count)) >= 1.0e+6) ?
-        `${Math.round(Math.abs(Number(count)) / 1.0e+6)  }M`:
-        Math.round(Math.abs(Number(count)) >= 1.0e+3) ?
-          `${Math.round(Math.abs(Number(count)) / 1.0e+3)  }K`:
-          '';
+  dataCountFormat(value) {
+    return Number(value) >= 1.0e9
+      ? `${this.trimDigits(Number(value) / 1.0e9)}B`
+      : Math.round(Number(value)) >= 1.0e6
+        ? `${this.trimDigits(Number(value) / 1.0e6)}M`
+        : Math.round(Number(value)) >= 1.0e3
+          ? `${this.trimDigits(Number(value) / 1.0e3)}K`
+          : value;
+  },
+
+  trimDigits(value) {
+    const newVal = value.toString().substring(0, 4);
+    if (newVal.indexOf('.') === 3) {
+      return newVal.substring(0, 3);
+    }
+    return newVal;
+  },
+
+  circleRadius(numberOfDigits) {
+    if (numberOfDigits <= 2) {
+      return 12;
+    } else if (numberOfDigits === 3) {
+      return 16;
+    } else if (numberOfDigits === 4) {
+      return 24;
+    } else if (numberOfDigits === 5) {
+      return 36;
+    } else if (numberOfDigits >= 6) {
+      return 48;
+    }
   }
-
-
 });
